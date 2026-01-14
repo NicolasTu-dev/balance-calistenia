3e1d3ae3aa4cca226a6f02503df911b4720a9f26
Nico <83230980+NicolasTu-dev@users.noreply.github.com>
2026-01-14 13:16:10 -0300

    fix redirect link

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -16,7 +16,7 @@ export default function LoginPage() {
 
     const redirectTo =
       typeof window !== "undefined"
-        ? `${window.location.origin}/auth/callback?next=/app`
+        ? `${window.location.origin}/auth/callback?next=/app/app`
         : undefined;
 
     const { error } = await supabase.auth.signInWithOtp({
